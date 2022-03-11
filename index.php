c4c5c2397af50316eeec8116ddd7ec202adffa1c
emusimbwa <etshamusimbwa@gmail.com>
2022-03-11 11:53:26 +0100

    premier commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    $age = 14;
+    $compteur = 1;
+
+    while($age < 18){
+        echo "Vous avez $age ans. Le temps passe...<br><br>";
+        $age++;
+    }
+
+    echo "$age ans! Vous êtes majeur"
+
+?>
